be40bf23e894d3773a51a0de84075c8675927e98
steve <shade@chemlab.org>
2010-09-23 14:47:08 -0400

    vision: Initial commit of device configuration.
    Change-Id: I12122ad2d44c027530c1827a240cad029dc73456

---
+++ extract-files.sh
@@ -0,0 +1,195 @@
+#!/bin/sh
+
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE=vision
+
+mkdir -p ../../../vendor/htc/$DEVICE/proprietary
+
+adb pull /system/bin/akmd ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/bin/awb_camera ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/bin/bma150_usr ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/bin/htc_ebdlogd ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/bin/logcat2 ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/bin/lsc_camera ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/A1026_CFG.csv ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/AdieHWCodec.csv ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/AdieHWCodec_WA.csv ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/agps_rm ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/AudioBTID.csv ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/bcm4329.hcd ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/bcm_mfg.bin ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/default.acdb ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/default_org.acdb ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/default_org_WA.acdb ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/fw_bcm4329_apsta.bin ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_command_control.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_h263_dec_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_h264_dec_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_h264_enc_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_mp2_dec_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_mp4_dec_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_mp4_enc_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/vidc_720p_vc1_dec_mc.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/Vision_SPK.acdb ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/yamato_pfp.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/yamato_pm4.fw ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/mm_property.conf ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/pvasflocal.cfg ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/vomeComp.cfg ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/vomeplay.cfg ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/voVidDec.dat ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/vpimg ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/egl/libEGL_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/egl/libGLESv1_CM_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/egl/libGLESv2_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/egl/libq3dtools_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libcamera.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libgemini.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libgsl.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libhtc_acoustic.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libHTC_mm_property.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libhtc_ril.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libloc_api-rpc.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libloc_api.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libmmipl.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libmmjpeg.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libmm-omxcore.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/liboemcamera.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libOmxCore.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libOmxVdec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libOmxVenc.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libomx_wmadec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libomx_wmvdec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libqcomm_omx.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoAACDec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoAMRNBDec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoAMRWBDec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoAndroid.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoH264Dec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoicesearch.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvomemedia.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoMMCCRRS.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoMP3Dec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoMPEG4Dec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoOMXME.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoOMXOne.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoPackUV.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvorbisidec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoSrcRTSP.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libvoVidDec.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/libysshared.so ../../../vendor/htc/$DEVICE/proprietary
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/htc/$DEVICE/$DEVICE-vendor-blobs.mk
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is generated by device/htc/__DEVICE__/extract-files.sh
+
+# Prebuilt libraries that are needed to build open-source libraries
+PRODUCT_COPY_FILES += \\
+    vendor/htc/__DEVICE__/proprietary/libloc_api.so:obj/lib/libloc_api.so \\
+    vendor/htc/__DEVICE__/proprietary/libloc_api-rpc.so:obj/lib/libloc_api-rpc.so \\
+    vendor/htc/__DEVICE__/proprietary/libcamera.so:obj/lib/libcamera.so
+
+# All the blobs necessary for passion
+PRODUCT_COPY_FILES += \\
+    vendor/htc/__DEVICE__/proprietary/A1026_CFG.csv:/system/etc/A1026_CFG.csv \\
+    vendor/htc/__DEVICE__/proprietary/AdieHWCodec.csv:/system/etc/AdieHWCodec.csv \\
+    vendor/htc/__DEVICE__/proprietary/AdieHWCodec_WA.csv:/system/etc/AdieHWCodec_WA.csv \\
+    vendor/htc/__DEVICE__/proprietary/agps_rm:/system/etc/agps_rm \\
+    vendor/htc/__DEVICE__/proprietary/akmd:/system/bin/akmd \\
+    vendor/htc/__DEVICE__/proprietary/AudioBTID.csv:/system/etc/AudioBTID.csv \\
+    vendor/htc/__DEVICE__/proprietary/awb_camera:/system/bin/awb_camera \\
+    vendor/htc/__DEVICE__/proprietary/bcm4329.hcd:/system/etc/firmware/bcm4329.hcd \\
+    vendor/htc/__DEVICE__/proprietary/bcm_mfg.bin:/system/etc/firmware/bcm_mfg.bin \\
+    vendor/htc/__DEVICE__/proprietary/bma150_usr:/system/bin/bma150_usr \\
+    vendor/htc/__DEVICE__/proprietary/default.acdb:/system/etc/firmware/default.acdb \\
+    vendor/htc/__DEVICE__/proprietary/default_org.acdb:/system/etc/firmware/default_org.acdb \\
+    vendor/htc/__DEVICE__/proprietary/default_org_WA.acdb:/system/etc/firmware/default_org_WA.acdb \\
+    vendor/htc/__DEVICE__/proprietary/fw_bcm4329_apsta.bin:/system/etc/firmware/fw_bcm4329_apsta.bin \\
+    vendor/htc/__DEVICE__/proprietary/htc_ebdlogd:/system/bin/htc_ebdlogd \\
+    vendor/htc/__DEVICE__/proprietary/libcamera.so:/system/lib/libcamera.so \\
+    vendor/htc/__DEVICE__/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \\
+    vendor/htc/__DEVICE__/proprietary/libgemini.so:/system/lib/libgemini.so \\
+    vendor/htc/__DEVICE__/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \\
+    vendor/htc/__DEVICE__/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \\
+    vendor/htc/__DEVICE__/proprietary/libgsl.so:/system/lib/libgsl.so \\
+    vendor/htc/__DEVICE__/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \\
+    vendor/htc/__DEVICE__/proprietary/libHTC_mm_property.so:/system/lib/libHTC_mm_property.so \\
+    vendor/htc/__DEVICE__/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \\
+    vendor/htc/__DEVICE__/proprietary/libloc_api-rpc.so:/system/lib/libloc_api-rpc.so \\
+    vendor/htc/__DEVICE__/proprietary/libloc_api.so:/system/lib/libloc_api.so \\
+    vendor/htc/__DEVICE__/proprietary/libmmipl.so:/system/lib/libmmipl.so \\
+    vendor/htc/__DEVICE__/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \\
+    vendor/htc/__DEVICE__/proprietary/libmm-omxcore.so:/system/lib/libmm-omxcore.so \\
+    vendor/htc/__DEVICE__/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \\
+    vendor/htc/__DEVICE__/proprietary/libOmxCore.so:/system/lib/libOmxCore.so \\
+    vendor/htc/__DEVICE__/proprietary/libOmxVdec.so:/system/lib/libOmxVdec.so \\
+    vendor/htc/__DEVICE__/proprietary/libOmxVenc.so:/system/lib/libOmxVenc.so \\
+    vendor/htc/__DEVICE__/proprietary/libomx_wmadec_sharedlibrary.so:/system/lib/libomx_wmadec_sharedlibrary.so \\
+    vendor/htc/__DEVICE__/proprietary/libomx_wmvdec_sharedlibrary.so:/system/lib/libomx_wmvdec_sharedlibrary.so \\
+    vendor/htc/__DEVICE__/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \\
+    vendor/htc/__DEVICE__/proprietary/libqcomm_omx.so:/system/lib/libqcomm_omx.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoAACDec.so:/system/lib/libvoAACDec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoAMRNBDec.so:/system/lib/libvoAMRNBDec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoAMRWBDec.so:/system/lib/libvoAMRWBDec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoAndroid.so:/system/lib/libvoAndroid.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoH264Dec.so:/system/lib/libvoH264Dec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoicesearch.so:/system/lib/libvoicesearch.so \\
+    vendor/htc/__DEVICE__/proprietary/libvomemedia.so:/system/lib/libvomemedia.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoMMCCRRS.so:/system/lib/libvoMMCCRRS.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoMP3Dec.so:/system/lib/libvoMP3Dec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoMPEG4Dec.so:/system/lib/libvoMPEG4Dec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoOMXME.so:/system/lib/libvoOMXME.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoOMXOne.so:/system/lib/libvoOMXOne.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoPackUV.so:/system/lib/libvoPackUV.so \\
+    vendor/htc/__DEVICE__/proprietary/libvorbisidec.so:/system/lib/libvorbisidec.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoSrcRTSP.so:/system/lib/libvoSrcRTSP.so \\
+    vendor/htc/__DEVICE__/proprietary/libvoVidDec.so:/system/lib/libvoVidDec.so \\
+    vendor/htc/__DEVICE__/proprietary/libysshared.so:/system/lib/libysshared.so \\
+    vendor/htc/__DEVICE__/proprietary/logcat2:/system/bin/logcat2 \\
+    vendor/htc/__DEVICE__/proprietary/lsc_camera:/system/bin/lsc_camera \\
+    vendor/htc/__DEVICE__/proprietary/mm_property.conf:/system/etc/mm_property.conf \\
+    vendor/htc/__DEVICE__/proprietary/pvasflocal.cfg:/system/etc/pvasflocal.cfg \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_command_control.fw:/system/etc/firmware/vidc_720p_command_control.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_h263_dec_mc.fw:/system/etc/firmware/vidc_720p_h263_dec_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_h264_dec_mc.fw:/system/etc/firmware/vidc_720p_h264_dec_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_h264_enc_mc.fw:/system/etc/firmware/vidc_720p_h264_enc_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_mp2_dec_mc.fw:/system/etc/firmware/vidc_720p_mp2_dec_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_mp4_dec_mc.fw:/system/etc/firmware/vidc_720p_mp4_dec_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_mp4_enc_mc.fw:/system/etc/firmware/vidc_720p_mp4_enc_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/vidc_720p_vc1_dec_mc.fw:/system/etc/firmware/vidc_720p_vc1_dec_mc.fw \\
+    vendor/htc/__DEVICE__/proprietary/Vision_SPK.acdb:/system/etc/firmware/Vision_SPK.acdb \\
+    vendor/htc/__DEVICE__/proprietary/vomeComp.cfg:/system/etc/vomeComp.cfg \\
+    vendor/htc/__DEVICE__/proprietary/vomeplay.cfg:/system/etc/vomeplay.cfg \\
+    vendor/htc/__DEVICE__/proprietary/voVidDec.dat:/system/etc/voVidDec.dat \\
+    vendor/htc/__DEVICE__/proprietary/vpimg:/system/etc/vpimg \\
+    vendor/htc/__DEVICE__/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \\
+    vendor/htc/__DEVICE__/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw
+EOF
+
+./setup-makefiles.sh
---
+++ vision.mk
@@ -0,0 +1,152 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM passion,
+# not specialized for any geography.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    device/htc/vision/gps.conf:system/etc/gps.conf
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/htc/vision/init.vision.rc:root/init.vision.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/vision/vision-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-hms-tmobile-us \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.2_r5 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/vision/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/vision/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    sensors.vision \
+    lights.vision
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/vision/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/htc/vision/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-ell.kcm.bin:system/usr/keychars/vision-keypad-ell.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-fra.kcm.bin:system/usr/keychars/vision-keypad-fra.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-ger.kcm.bin:system/usr/keychars/vision-keypad-ger.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-ita.kcm.bin:system/usr/keychars/vision-keypad-ita.kcm.bin \
+    device/htc/vision/keychars/vision-keypad.kcm.bin:system/usr/keychars/vision-keypad.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-tur.kcm.bin:system/usr/keychars/vision-keypad-tur.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-wwe-bopomo.kcm.bin:system/usr/keychars/vision-keypad-wwe-bopomo.kcm.bin \
+    device/htc/vision/keychars/vision-keypad-wwe.kcm.bin:system/usr/keychars/vision-keypad-wwe.kcm.bin \
+    device/htc/vision/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/vision/keylayout/vision-keypad-ara.kl:system/usr/keylayout/vision-keypad-ara.kl \
+    device/htc/vision/keylayout/vision-keypad-ell.kl:system/usr/keylayout/vision-keypad-ell.kl \
+    device/htc/vision/keylayout/vision-keypad-fra.kl:system/usr/keylayout/vision-keypad-fra.kl \
+    device/htc/vision/keylayout/vision-keypad-ger.kl:system/usr/keylayout/vision-keypad-ger.kl \
+    device/htc/vision/keylayout/vision-keypad-ita.kl:system/usr/keylayout/vision-keypad-ita.kl \
+    device/htc/vision/keylayout/vision-keypad.kl:system/usr/keylayout/vision-keypad.kl \
+    device/htc/vision/keylayout/vision-keypad-tur.kl:system/usr/keylayout/vision-keypad-tur.kl \
+    device/htc/vision/keylayout/vision-keypad-wwe-bopomo.kl:system/usr/keylayout/vision-keypad-wwe-bopomo.kl \
+    device/htc/vision/keylayout/vision-keypad-wwe.kl:system/usr/keylayout/vision-keypad-wwe.kl
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/vision/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/vision/firmware/bcm_mfg.bin:system/etc/firmware/bcm_mfg.bin \
+    device/htc/vision/firmware/default.acdb:system/etc/firmware/default.acdb \
+    device/htc/vision/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
+    device/htc/vision/firmware/default_org_WA.acdb:system/etc/firmware/default_org_WA.acdb \
+    device/htc/vision/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
+    device/htc/vision/firmware/vidc_720p_command_control.fw:system/etc/firmware/vidc_720p_command_control.fw \
+    device/htc/vision/firmware/vidc_720p_h263_dec_mc.fw:system/etc/firmware/vidc_720p_h263_dec_mc.fw \
+    device/htc/vision/firmware/vidc_720p_h264_dec_mc.fw:system/etc/firmware/vidc_720p_h264_dec_mc.fw \
+    device/htc/vision/firmware/vidc_720p_h264_enc_mc.fw:system/etc/firmware/vidc_720p_h264_enc_mc.fw \
+    device/htc/vision/firmware/vidc_720p_mp2_dec_mc.fw:system/etc/firmware/vidc_720p_mp2_dec_mc.fw \
+    device/htc/vision/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
+    device/htc/vision/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
+    device/htc/vision/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
+    device/htc/vision/firmware/Vision_SPK.acdb:system/etc/firmware/Vision_SPK.acdb \
+    device/htc/vision/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/htc/vision/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.f
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# vision uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/vision/vold.fstab:system/etc/vold.fstab
+
+# Kernel modules
+#PRODUCT_COPY_FILES += \
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/vision/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES += \
+    device/htc/vision/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
+
+$(call inherit-product-if-exists, vendor/htc/vision/vision-vendor.mk)
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/vision/media_a1026.mk)
+
+# htc audio settings
+$(call inherit-product, device/htc/vision/media_htcaudio.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full.mk)
+
+
+PRODUCT_NAME := htc_vision
+PRODUCT_DEVICE := vision
+PRODUCT_MODEL := T-Mobile G2
+PRODUCT_MANUFACTURER := HTC
+
